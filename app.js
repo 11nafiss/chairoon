f232486e83912ada9b87a5d68bf22f3c6ab9ee56
Muhammad Nafis <m.nafis.2001@gmail.com>
2024-06-06 21:31:10 +0700

    update header access

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.options("", cors(corsConfig));
 app.use(cookieParser());
 
 app.use((req,res,next)=>{
-  res.setHeader('Access-Control-Allow-Origin','*');
+  res.setHeader('Access-Control-Allow-Origin',`${process.env.CLIENT_URL}`);
   res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
   res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
   next(); 
