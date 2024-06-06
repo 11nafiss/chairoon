c056924c80565524976dd90cc33cd0537cea1174
Muhammad Nafis <m.nafis.2001@gmail.com>
2024-06-06 21:54:37 +0700

    update socket client

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,13 +1,20 @@
 import io from "socket.io-client"; // Add this
 // config
-import { BASE_URL } from './config';
+import { BASE_URL } from "./config";
 
 let socket;
 
 const connectSocket = (user_id) => {
   socket = io(`${BASE_URL}`, {
     query: `user_id=${user_id}`,
+    transportOptions: {
+      polling: {
+        extraHeaders: {
+          "x-client-header": "value", // Add any required headers here
+        },
+      },
+    },
   });
-} // Add this -- our server will run on port 4000, so we connect to it from here
+}; // Add this -- our server will run on port 4000, so we connect to it from here
 
-export {socket, connectSocket};
+export { socket, connectSocket };
