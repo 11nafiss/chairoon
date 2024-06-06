5097fbfa3199701f017254900d2d1dd5f3d58ba1
Muhammad Nafis <m.nafis.2001@gmail.com>
2024-06-06 21:12:27 +0700

    update socket

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,13 +1,13 @@
-import io from "socket.io-client"; // Add this
-// config
-import { BASE_URL } from './config';
+// import io from "socket.io-client"; // Add this
+// // config
+// import { BASE_URL } from './config';
 
-let socket;
+// let socket;
 
-const connectSocket = (user_id) => {
-  socket = io(`${BASE_URL}`, {
-    query: `user_id=${user_id}`,
-  });
-} // Add this -- our server will run on port 4000, so we connect to it from here
+// const connectSocket = (user_id) => {
+//   socket = io(`${BASE_URL}`, {
+//     query: `user_id=${user_id}`,
+//   });
+// } // Add this -- our server will run on port 4000, so we connect to it from here
 
-export {socket, connectSocket};
+// export {socket, connectSocket};
