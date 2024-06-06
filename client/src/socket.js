23fbcb3e098c818d56367e382f151cf6633877c3
Muhammad Nafis <m.nafis.2001@gmail.com>
2024-06-06 21:56:25 +0700

    update socket error

--- client/src/socket.js
+++ client/src/socket.js
@@ -7,13 +7,7 @@ let socket;
 const connectSocket = (user_id) => {
   socket = io(`${BASE_URL}`, {
     query: `user_id=${user_id}`,
-    transportOptions: {
-      polling: {
-        extraHeaders: {
-          "x-client-header": "value", // Add any required headers here
-        },
-      },
-    },
+    transports: ['polling']
   });
 }; // Add this -- our server will run on port 4000, so we connect to it from here
 
